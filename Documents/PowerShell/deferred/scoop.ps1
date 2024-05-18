6aa1e592203b809607e931634eedef557e05d49f
seanmamasde <seanmamasde@gmail.com>
2024-05-19 00:39:19 +0800

    Update Documents/PowerShell/deferred/scoop.ps1

--- Documents/PowerShell/deferred/scoop.ps1
+++ Documents/PowerShell/deferred/scoop.ps1
@@ -1,10 +1,10 @@
 # Import-Module scoop-completion
 
 # scoop-search: https://github.com/shilangyu/scoop-search
-# Invoke-Expression (& scoop-search --hook)
+Invoke-Expression (& scoop-search --hook)
 
 # scoop commands replacements
-Invoke-Expression (&sfsu hook --disable list status)
+# Invoke-Expression (&sfsu hook --disable list status)
 
 # enable scoop completion in current shell, use absolute path because PowerShell Core not respect $env:PSModulePath
-# Import-Module "$($(Get-Item $(Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion"
+Import-Module "$($(Get-Item $(Get-Command scoop.ps1).Path).Directory.Parent.FullName)\modules\scoop-completion"
