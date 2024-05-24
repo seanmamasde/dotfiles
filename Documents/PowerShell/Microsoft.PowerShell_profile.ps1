6048a57db7d5a4f2d3d5fc583140085f0e65c66e
seanmamasde <seanmamasde@gmail.com>
2024-05-24 18:51:43 +0800

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1 Add Documents/PowerShell/deferred/conda.ps1 Remove Documents/PowerShell/deferred/conda.ps1

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -78,26 +78,6 @@ if ($PSVersionTable.OS -like '*Windows*' -and $host.UI.SupportsVirtualTerminal) 
   }
 }
 
-# conda
-# alternative conda activation method (https://github.com/conda/conda/issues/11648#issuecomment-1541546403)
-# mamba activation (https://github.com/mamba-org/mamba/issues/1717#issuecomment-1292845827)
-$Env:CONDA_EXE = "C:\Users\seanma\scoop\apps\miniconda3\current\Scripts\conda.exe"
-# $ENV:MAMBA_EXE = "C:\Users\seanma\scoop\apps\miniconda3\current\Scripts\mamba.exe"  # for mamba
-$Env:_CE_M = ""
-$Env:_CE_CONDA = ""
-$Env:_CONDA_ROOT = "C:\Users\seanma\scoop\apps\miniconda3\current"
-$Env:_CONDA_EXE = "C:\Users\seanma\scoop\apps\miniconda3\current\Scripts\conda.exe"
-$CondaModuleArgs = @{ChangePs1 = $False}
-Import-Module "$Env:_CONDA_ROOT\shell\condabin\Conda.psm1" -ArgumentList $CondaModuleArgs
-# Import-Module "$Env:_CONDA_ROOT\shell\condabin\Mamba.psm1" -ArgumentList $CondaModuleArgs # for mamba
-Remove-Variable CondaModuleArgs
-
-# conda activate base
-
-# enable thefuck
-# $env:PYTHONIOENCODING='utf-8'
-# iex "$(thefuck --alias --enable-experimental-instant-mode)"
-
 # starship
 if ($env:TERM_PROGRAM -ne 'vscode') {
   $ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
