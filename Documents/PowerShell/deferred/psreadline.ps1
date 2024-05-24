700c52578299a99ecfd2be029d60a0159cca230c
seanmamasde <seanmamasde@gmail.com>
2024-05-24 11:06:29 +0800

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1 Remove Documents/PowerShell/deferred/conda.ps1 Add Documents/PowerShell/deferred/conda.ps1 Update Documents/PowerShell/deferred/psreadline.ps1

--- Documents/PowerShell/deferred/psreadline.ps1
+++ Documents/PowerShell/deferred/psreadline.ps1
@@ -11,53 +11,45 @@ Set-PSReadLineKeyHandler -Key Shift+Delete `
 -BriefDescription RemoveFromHistory `
 -LongDescription "Removes the content of the current line from history" `
 -ScriptBlock {
-  param($key, $arg)
+    param($key, $arg)
 
-  $line = $null
-  $cursor = $null
-  [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+    $line = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
 
-  $toRemove = [Regex]::Escape(($line -replace "\n", "```n"))
-  $history = Get-Content (Get-PSReadLineOption).HistorySavePath -Raw
-  $history = $history -replace "(?m)^$toRemove\r\n", ""
-  Set-Content (Get-PSReadLineOption).HistorySavePath $history
+    $toRemove = [Regex]::Escape(($line -replace "\n", "```n"))
+    $history = Get-Content (Get-PSReadLineOption).HistorySavePath -Raw
+    $history = $history -replace "(?m)^$toRemove\r\n", ""
+    Set-Content (Get-PSReadLineOption).HistorySavePath $history
 }
 
 # from https://github.com/benallred/configs/blob/f8814951e66d33feb4ed045b456a2ca60bd46658/powershell/PSReadLine.ps1 
-# smart braces
 # From https://github.com/PowerShell/PSReadLine/blob/master/PSReadLine/SamplePSReadLineProfile.ps1 with some changes
-
+# smart braces
 # Set-PSReadLineKeyHandler -Key '"', "'" `
-#     -BriefDescription SmartInsertQuote `
-#     -LongDescription "Insert paired quotes if not already on a quote" `
-#     -ScriptBlock {
+# -BriefDescription SmartInsertQuote `
+# -LongDescription "Insert paired quotes if not already on a quote" `
+# -ScriptBlock {
 #     param($key, $arg)
-# 
 #     $quote = $key.KeyChar
-# 
 #     $selectionStart = $null
 #     $selectionLength = $null
 #     [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$selectionStart, [ref]$selectionLength)
-# 
 #     $line = $null
 #     $cursor = $null
 #     [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
-# 
 #     # If text is selected, just quote it without any smarts
 #     if ($selectionStart -ne -1) {
 #         [Microsoft.PowerShell.PSConsoleReadLine]::Replace($selectionStart, $selectionLength, $quote + $line.SubString($selectionStart, $selectionLength) + $quote)
 #         [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selectionStart + $selectionLength + 2)
 #         return
 #     }
-# 
 #     $ast = $null
 #     $tokens = $null
 #     $parseErrors = $null
 #     [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$parseErrors, [ref]$null)
-# 
 #     function FindToken {
 #         param($tokens, $cursor)
-# 
 #         foreach ($token in $tokens) {
 #             if ($cursor -lt $token.Extent.StartOffset) { continue }
 #             if ($cursor -lt $token.Extent.EndOffset) {
@@ -67,15 +59,12 @@ Set-PSReadLineKeyHandler -Key Shift+Delete `
 #                     $nested = FindToken $token.NestedTokens $cursor
 #                     if ($nested) { $result = $nested }
 #                 }
-# 
 #                 return $result
 #             }
 #         }
 #         return $null
 #     }
-# 
 #     $token = FindToken $tokens $cursor
-# 
 #     # If we're on or inside a **quoted** string token (so not generic), we need to be smarter
 #     if ($token -is [StringToken] -and $token.Kind -ne [TokenKind]::Generic) {
 #         # If we're at the start of the string, assume we're inserting a new string
@@ -84,14 +73,12 @@ Set-PSReadLineKeyHandler -Key Shift+Delete `
 #             [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
 #             return
 #         }
-# 
 #         # If we're at the end of the string, move over the closing quote if present.
 #         if ($token.Extent.EndOffset -eq ($cursor + 1) -and $line[$cursor] -eq $quote) {
 #             [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
 #             return
 #         }
 #     }
-# 
 #     if ($null -eq $token -or
 #         $token.Kind -eq [TokenKind]::RParen -or $token.Kind -eq [TokenKind]::RCurly -or $token.Kind -eq [TokenKind]::RBracket) {
 #         if ($line[0..$cursor].Where{ $_ -eq $quote }.Count % 2 -eq 1) {
@@ -105,7 +92,6 @@ Set-PSReadLineKeyHandler -Key Shift+Delete `
 #         }
 #         return
 #     }
-# 
 #     # If cursor is at the start of a token, enclose it in quotes.
 #     if ($token.Extent.StartOffset -eq $cursor) {
 #         if ($token.Kind -eq [TokenKind]::Generic -or $token.Kind -eq [TokenKind]::Identifier -or
@@ -117,15 +103,14 @@ Set-PSReadLineKeyHandler -Key Shift+Delete `
 #             return
 #         }
 #     }
-# 
 #     # We failed to be smart, so just insert a single quote
 #     [Microsoft.PowerShell.PSConsoleReadLine]::Insert($quote)
 # }
 
 Set-PSReadLineKeyHandler -Key '(', '{', '[' `
-    -BriefDescription InsertPairedBraces `
-    -LongDescription "Insert matching braces" `
-    -ScriptBlock {
+-BriefDescription InsertPairedBraces `
+-LongDescription "Insert matching braces" `
+-ScriptBlock {
     param($key, $arg)
 
     $closeChar = switch ($key.KeyChar) {
@@ -155,9 +140,9 @@ Set-PSReadLineKeyHandler -Key '(', '{', '[' `
 }
 
 Set-PSReadLineKeyHandler -Key ')', ']', '}' `
-    -BriefDescription SmartCloseBraces `
-    -LongDescription "Insert closing brace or skip" `
-    -ScriptBlock {
+-BriefDescription SmartCloseBraces `
+-LongDescription "Insert closing brace or skip" `
+-ScriptBlock {
     param($key, $arg)
 
     $line = $null
@@ -173,9 +158,9 @@ Set-PSReadLineKeyHandler -Key ')', ']', '}' `
 }
 
 Set-PSReadLineKeyHandler -Key Backspace `
-    -BriefDescription SmartBackspace `
-    -LongDescription "Delete previous character or matching quotes/parens/braces" `
-    -ScriptBlock {
+-BriefDescription SmartBackspace `
+-LongDescription "Delete previous character or matching quotes/parens/braces" `
+-ScriptBlock {
     param($key, $arg)
 
     $line = $null
