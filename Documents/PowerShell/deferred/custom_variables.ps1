7bb70bab82a123c760753f9e9bb41c089d255e75
seanmamasde <seanmamasde@gmail.com>
2024-06-02 03:19:28 +0800

    Update AppData/Local/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/settings.json Update Documents/PowerShell/deferred/custom_variables.ps1

--- Documents/PowerShell/deferred/custom_variables.ps1
+++ Documents/PowerShell/deferred/custom_variables.ps1
@@ -3,11 +3,12 @@ $deferred_dir = "$HOME\Documents\PowerShell\deferred"
 # chezmoi related
 $chezmoi_dir = "$HOME\.local\share\chezmoi"
 $chezmoi_paths = @(
-  "$HOME/.condarc", # condarc
-  "$HOME/.gitconfig", # gitconfig
-  "$HOME/AppData/Local/Packages/Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe/LocalState/settings.json", # windows terminal settings.json
-  "$HOME/README.md",  # readme of the repo
-  "$HOME/source/scripts"  # custom scripts  
+  "$profile",
+  "$deferred_dir",
+  "$HOME\.condarc", # condarc
+  "$HOME\.gitconfig", # gitconfig
+  "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json", # windows terminal settings.json
+  "$HOME\README.md",  # readme of the repo
+  "$HOME\source\scripts"  # custom scripts
 )
-$chezmoi_all = $chezmoi_paths -join ' '
 
