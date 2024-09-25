147e0070da9ad5a079a2a991601a8243dfcb0806
seanmamasde <seanmamasde@gmail.com>
2024-09-26 02:20:43 +0800

    Add source/scripts/conda_mamba/conda-hook.ps1

---
+++ source/scripts/conda_mamba/conda-hook.ps1
@@ -0,0 +1,9 @@
+$Env:CONDA_EXE = "C:\Users\seanma\miniforge3\Scripts\conda.exe"
+$Env:_CE_M = ""
+$Env:_CE_CONDA = ""
+$Env:_CONDA_ROOT = "C:\Users\seanma\miniforge3"
+$Env:_CONDA_EXE = "C:\Users\seanma\miniforge3\Scripts\conda.exe"
+$CondaModuleArgs = @{ChangePs1 = $False}
+Import-Module "$Env:_CONDA_ROOT\shell\condabin\Conda.psm1" -ArgumentList $CondaModuleArgs
+
+Remove-Variable CondaModuleArgs
